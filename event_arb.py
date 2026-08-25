b624721b5af87d0a557148521d49a8a212758a28
Timothy James Granderson <tgranderson@pvamu.edu>
2026-08-25 15:38:29 -0500

    Refactor arbitrage system

--- event_arb.py
+++ event_arb.py
@@ -1,40 +1,73 @@
 import json
 import arb_math
 
-data = open("test_data.json").read()
-payload = json.loads(data)
 
-event = payload["events"][0]
+def load_event(filename="test_data.json"):
+    data = open(filename).read()
+    payload = json.loads(data)
+    return payload["events"][0]
 
-away = None
-home = None
 
-for team in event["teams"]:
-    if team["is_away"]:
-        away = team
-    if team["is_home"]:
-        home = team
+def get_teams(event):
+    away = None
+    home = None
 
-away_odds = []
-home_odds = []
+    for team in event["teams"]:
+        if team["is_away"]:
+            away = team
+        if team["is_home"]:
+            home = team
 
-for market in event["odds"]["moneyline"]:
-    away_odds.append(market[away["abbreviation"]])
-    home_odds.append(market[home["abbreviation"]])
+    return away, home
 
-best_away = max(away_odds)
-best_home = max(home_odds)
 
-decimal_away = arb_math.american_to_decimal(best_away)
-decimal_home = arb_math.american_to_decimal(best_home)
+def get_best_odds(event, away, home):
+    best_away = None
+    best_home = None
 
-arb = arb_math.arb_index(decimal_away, decimal_home)
+    for market in event["odds"]["moneyline"]:
+        away_odds = market[away["abbreviation"]]
+        home_odds = market[home["abbreviation"]]
 
-print ""
-print away["name"], "@", home["name"]
-print ""
-print "Best", away["abbreviation"], ":", best_away
-print "Best", home["abbreviation"], ":", best_home
-print ""
-print "Arbitrage Percentage:", arb * 100, "%"
-print "Arbitrage:", arb < 1
+        if best_away is None or away_odds > best_away:
+            best_away = away_odds
+
+        if best_home is None or home_odds > best_home:
+            best_home = home_odds
+
+    return best_away, best_home
+
+
+def analyze_event(event):
+    away, home = get_teams(event)
+
+    best_away, best_home = get_best_odds(
+        event,
+        away,
+        home
+    )
+
+    decimal_away = arb_math.american_to_decimal(best_away)
+    decimal_home = arb_math.american_to_decimal(best_home)
+
+    arb = arb_math.arb_index(
+        decimal_away,
+        decimal_home
+    )
+
+    return {
+        "away": away,
+        "home": home,
+        "best_away": best_away,
+        "best_home": best_home,
+        "arb": arb
+    }
+
+
+def main():
+    event = load_event()
+    return analyze_event(event)
+
+
+if __name__ == "__main__":
+    main()
